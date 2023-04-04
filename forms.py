fba483da6aebb33ec731df4f96b5c0a1af4adee3
RayaanQ <rayyaanquraishi@gmail.com>
2023-04-04 15:30:01 +0530

    added validators

--- forms.py
+++ forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAr
 from wtforms.validators import DataRequired,Length,Email,EqualTo
 
 class ContactForm(FlaskForm):
-    firstname = StringField('firstname')
+    firstname = StringField('firstname',validators=[DataRequired(), Length(min=2,max=20)])
     
-    lastname = StringField('lastname')
+    lastname = StringField('lastname',validators=[DataRequired(), Length(min=2,max=20)])
     
-    email = StringField('email')
+    email = StringField('email',validators=[DataRequired(), Email()])
     
-    number = StringField('number')
+    number = StringField('number',validators=[DataRequired()])
     
-    residence = StringField('residence')
+    residence = StringField('residence',validators=[DataRequired()])
     
     # reason = SelectMultipleField('reason', choices=["Make an enquiry","Give feedback and suggestions","Request for business and meeting information","Give compliments","Submit a complaint"],validate_choice=False)
     
